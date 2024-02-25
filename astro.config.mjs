9245621c8bdb241b610ce4d2a14c7352bd83af9a
erick <erick.olviares.gonzalez@gmail.com>
2024-02-25 16:31:29 -0300

    github workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-   
-    base:"/",
-    outDir:"./docs",
+    site:"https://VOID1499.github.io",
     integrations:[tailwind()],
 });
