8a4b2365156817d61884378ca29a533d34a6c221
erick <erick.olviares.gonzalez@gmail.com>
2024-02-26 19:56:03 -0300

    base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site:"https://VOID1499.github.io",
-    base:"void1499",
+    base:"/void1499",
     integrations:[tailwind()],
 });
